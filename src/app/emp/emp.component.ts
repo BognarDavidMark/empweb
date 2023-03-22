2829a06631eaabc1538479424d58a87034051fef
Bognár Dávid <bognardavidmark@gmail.com>
2023-03-22 09:51:11 +0100

    félkész

---
+++ src/app/emp/emp.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../shared/api.service';
+
+@Component({
+  selector: 'app-emp',
+  templateUrl: './emp.component.html',
+  styleUrls: ['./emp.component.scss']
+})
+export class EmpComponent implements OnInit {
+  employees = []
+
+  constructor( private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.getEmployee();
+  }
+
+  getEmployee() {
+    this.api.getEmployee().subscribe({
+      next: (result) => {
+        console.log(result)
+      },
+      error: (err) => {
+        console.log("Hiba!");
+      }
+      
+    });
+  }
+}
